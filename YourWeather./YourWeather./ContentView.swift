b16400eff37da7d007a93d09ca73b6238adc7faf
Kevin <choims1379@gmail.com>
2025-09-09 10:56:09 +0900

    Project Settings

---
+++ YourWeather./YourWeather./ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YourWeather.
+//
+//  Created by 최명수 on 2025/9/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YourWeather./YourWeather./YourWeather_App.swift
@@ -0,0 +1,17 @@
+//
+//  YourWeather_App.swift
+//  YourWeather.
+//
+//  Created by 최명수 on 2025/9/9.
+//
+
+import SwiftUI
+
+@main
+struct YourWeather_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
