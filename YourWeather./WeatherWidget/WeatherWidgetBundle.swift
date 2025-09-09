497f11101701e02de1bb5da454100f2a54083049
Kevin <choims1379@gmail.com>
2025-09-09 11:25:23 +0900

    K1 Features: Widget Extension Setting

---
+++ YourWeather./WeatherWidget/WeatherWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherWidgetBundle.swift
+//  WeatherWidget
+//
+//  Created by 최명수 on 2025/9/9.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WeatherWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WeatherWidget()
+        WeatherWidgetControl()
+        WeatherWidgetLiveActivity()
+    }
+}
